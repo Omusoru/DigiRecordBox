e35488a851d1bc6816ddf60b3c9e0f7a2023f6e6
razzzvan321 <razzzvan321@gmail.com>
2014-07-08 12:41:16 +0300

    Update logo si app name

--- gen/ro/rcsrds/recordbox/R.java
+++ gen/ro/rcsrds/recordbox/R.java
@@ -11,16 +11,42 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int digirecordbox=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int button1=0x7f060000;
+        public static final int btn_cancel=0x7f070008;
+        public static final int btn_login=0x7f070002;
+        public static final int btn_record=0x7f070006;
+        public static final int btn_stop=0x7f070007;
+        public static final int et_email=0x7f070000;
+        public static final int et_password=0x7f070001;
+        public static final int img_logo=0x7f070003;
+        public static final int option_menu_about=0x7f07000a;
+        public static final int option_menu_exit=0x7f07000b;
+        public static final int option_menu_login=0x7f070009;
+        public static final int tv_email=0x7f070004;
+        public static final int tv_password=0x7f070005;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int login=0x7f030000;
+        public static final int main=0x7f030001;
+    }
+    public static final class menu {
+        public static final int options_menu=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int btn_cancel=0x7f040004;
+        public static final int btn_login=0x7f040008;
+        public static final int btn_stop=0x7f040003;
+        public static final int option_menu_about=0x7f040006;
+        public static final int option_menu_exit=0x7f040007;
+        public static final int option_menu_login=0x7f040005;
+        public static final int tgl_pause=0x7f040002;
+        public static final int tgl_record=0x7f040001;
+        public static final int tv_email=0x7f040009;
+        public static final int tv_password=0x7f04000a;
     }
     public static final class style {
         /** 
