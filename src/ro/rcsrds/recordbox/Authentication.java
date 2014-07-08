2572b4afb62e6ddebacb5623f9166f94bbb7b46e
razzzvan321 <razzzvan321@gmail.com>
2014-07-08 22:08:46 +0300

    Modificat login system sa foloseasca FTP

--- src/ro/rcsrds/recordbox/Authentication.java
+++ src/ro/rcsrds/recordbox/Authentication.java
@@ -1,22 +1,21 @@
 package ro.rcsrds.recordbox;
 
-import net.koofr.api.v2.DefaultClientFactory;
-import net.koofr.api.v2.StorageApi;
-import net.koofr.api.v2.StorageApiException;
 import android.content.SharedPreferences;
 import android.util.Log;
 
 public class Authentication {
 
 	private String host = "storage.rcs-rds.ro";
-	private String authToken;
+	private int port = 21;
+	//private String authToken;
 	private boolean loggedIn;
 	private static final String TAG = "Login";
 	private SharedPreferences preferences;
+	private DigiFTPClient ftp;
 	
 	public Authentication(SharedPreferences preferences) {
 		this.loggedIn = preferences.getBoolean("loggedIn", false);
-		this.authToken = preferences.getString("authToken", "");
+		//this.authToken = preferences.getString("authToken", "");
 		this.preferences = preferences;
 	}
 	
@@ -24,31 +23,44 @@ public class Authentication {
 		return this.loggedIn;
 	}
 	
-	public String getAuthToken() {
-		return this.authToken;
-	}
-	
 	public boolean logIn(String username, String password) {
-		StorageApi api = null;
+		/*StorageApi api = null;
 		this.loggedIn = true;
 		try {
 			api = DefaultClientFactory.create(this.host,username, password);
 		} catch (StorageApiException sae) {
 			Log.e(Authentication.TAG,sae.getMessage());
 			this.loggedIn = false;
+		}*/
+		
+		ftp=new DigiFTPClient(this.host,this.port,username,password);
+		try {
+			ftp.connect();
+		} catch (Exception e) {
+			Log.d(Authentication.TAG,e.getMessage());
 		}
 		
+		this.loggedIn = ftp.logIn();
+		
 		if(this.loggedIn) {
-			this.authToken = api.getAuthToken();
+			//this.authToken = api.getAuthToken();
 			//Save login info to SharedPreferences	
 		    SharedPreferences.Editor editor = this.preferences.edit();
 		    editor.putBoolean("loggedIn", true);
-		    String authToken = api.getAuthToken();
-		    editor.putString("authToken", authToken);
+		    //String authToken = api.getAuthToken();
+		    //editor.putString("authToken", authToken);
 		    editor.commit();
 		    return true;
+		    
 		} else {
+			try {
+				ftp.disconnect();
+			} catch (Exception e) {
+				Log.d(Authentication.TAG, e.getMessage());
+			}
+			
 			return false;
+			
 		}
 		
 	}
@@ -56,13 +68,21 @@ public class Authentication {
 	public void logOut() {
 		
 		this.loggedIn = false;
-		this.authToken = "";
+		//this.authToken = "";
 		
 		// Clear login info
 	    SharedPreferences.Editor editor = preferences.edit();
 	    editor.putBoolean("loggedIn", false);
-	    editor.putString("authToken", "");
+	    //editor.putString("authToken", "");
 	    editor.commit();
+	    
+	    //Disconnect FTP
+	    try {
+	    	ftp.disconnect();
+	    } catch (Exception e) {
+	    	Log.d(Authentication.TAG,e.getMessage());
+	    }
+	    
 	}
 	
 }
