6b9b444e5d5e513aec7a4fa9c439ee931ff686d8
razzzvan321 <razzzvan321@gmail.com>
2014-07-09 13:05:42 +0300

    Added themes and styles

--- gen/ro/rcsrds/recordbox/R.java
+++ gen/ro/rcsrds/recordbox/R.java
@@ -10,59 +10,59 @@ package ro.rcsrds.recordbox;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int light=0x7f040000;
+    }
     public static final class drawable {
         public static final int digirecordbox=0x7f020000;
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int btn_cancel=0x7f070008;
-        public static final int btn_login=0x7f070002;
-        public static final int btn_record=0x7f070006;
-        public static final int btn_stop=0x7f070007;
-        public static final int et_email=0x7f070000;
-        public static final int et_password=0x7f070001;
-        public static final int img_logo=0x7f070003;
-        public static final int option_menu_about=0x7f07000a;
-        public static final int option_menu_exit=0x7f07000b;
-        public static final int option_menu_login=0x7f070009;
-        public static final int tv_email=0x7f070004;
-        public static final int tv_password=0x7f070005;
+        public static final int btn_login=0x7f080003;
+        public static final int btn_player_stop=0x7f08000b;
+        public static final int btn_recorder_cancel=0x7f080008;
+        public static final int btn_recorder_start=0x7f080006;
+        public static final int btn_recorder_stop=0x7f080007;
+        public static final int et_email=0x7f080001;
+        public static final int et_password=0x7f080005;
+        public static final int img_logo=0x7f080000;
+        public static final int option_menu_about=0x7f08000d;
+        public static final int option_menu_exit=0x7f08000e;
+        public static final int option_menu_logout=0x7f08000c;
+        public static final int option_menu_mediaplayer=0x7f08000f;
+        public static final int textView1=0x7f080009;
+        public static final int tgl_player_play=0x7f08000a;
+        public static final int tv_email=0x7f080002;
+        public static final int tv_password=0x7f080004;
     }
     public static final class layout {
         public static final int login=0x7f030000;
         public static final int main=0x7f030001;
+        public static final int mediaplayer=0x7f030002;
     }
     public static final class menu {
-        public static final int options_menu=0x7f060000;
+        public static final int main_options_menu=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int btn_cancel=0x7f040004;
-        public static final int btn_login=0x7f040008;
-        public static final int btn_stop=0x7f040003;
-        public static final int option_menu_about=0x7f040006;
-        public static final int option_menu_exit=0x7f040007;
-        public static final int option_menu_login=0x7f040005;
-        public static final int tgl_pause=0x7f040002;
-        public static final int tgl_record=0x7f040001;
-        public static final int tv_email=0x7f040009;
-        public static final int tv_password=0x7f04000a;
+        public static final int app_name=0x7f050000;
+        public static final int btn_login=0x7f05000c;
+        public static final int btn_player_stop=0x7f050007;
+        public static final int btn_recorder_cancel=0x7f050004;
+        public static final int btn_recorder_stop=0x7f050003;
+        public static final int message_authentication_failed=0x7f05000f;
+        public static final int option_menu_about=0x7f050009;
+        public static final int option_menu_exit=0x7f05000a;
+        public static final int option_menu_logout=0x7f050008;
+        public static final int option_menu_mediaplayer=0x7f05000b;
+        public static final int tgl_player_pause=0x7f050006;
+        public static final int tgl_player_play=0x7f050005;
+        public static final int tgl_recorder_pause=0x7f050002;
+        public static final int tgl_recorder_start=0x7f050001;
+        public static final int tv_email=0x7f05000d;
+        public static final int tv_password=0x7f05000e;
     }
     public static final class style {
-        /** 
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-    
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
-         */
-        public static final int AppBaseTheme=0x7f050000;
-        /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
-         */
-        public static final int AppTheme=0x7f050001;
+        public static final int DigiTheme=0x7f060000;
+        public static final int DigiTheme_Login=0x7f060001;
     }
 }
