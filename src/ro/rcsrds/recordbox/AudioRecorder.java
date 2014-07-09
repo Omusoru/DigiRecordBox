afca7f92581415b73f03c451fb6f6bea967e1d7a
Omusoru <Tybyro@gmail.com>
2014-07-09 16:57:23 +0300

    Am facut pause/resume la recording. -Tibi

--- src/ro/rcsrds/recordbox/AudioRecorder.java
+++ src/ro/rcsrds/recordbox/AudioRecorder.java
@@ -1,19 +1,28 @@
 package ro.rcsrds.recordbox;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.SequenceInputStream;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.coremedia.iso.boxes.Container;
+import com.googlecode.mp4parser.authoring.Movie;
+import com.googlecode.mp4parser.authoring.Track;
+import com.googlecode.mp4parser.authoring.builder.DefaultMp4Builder;
+import com.googlecode.mp4parser.authoring.container.mp4.MovieCreator;
+import com.googlecode.mp4parser.authoring.tracks.AppendTrack;
 
 import android.annotation.SuppressLint;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Environment;
 import android.util.Log;
+
 //Pentru a incepe o inregistrare apelati startRecording, pentru a o incheia apelati stopRecording.
 //Aveti grija sa nu apelati starRecording in timp ce inregistreaza(Ex:dati disable la butonu de record si renable la stop)
 @SuppressLint("SimpleDateFormat")
@@ -37,28 +46,28 @@ public class AudioRecorder {
     private int playingPausedAt;
     private int recPaused = 0;
     private int recMerged = 0;
+	private RandomAccessFile randomAccessFile;
     
-    
-    //Setam directorul unde se salveaza fisierul
+	//Setam directorul unde se salveaza fisierul
     public AudioRecorder() {
-        filePath = Environment.getExternalStorageDirectory().getAbsolutePath();        
+        filePath = Environment.getExternalStorageDirectory().getAbsolutePath()+"/DigiRecordBox";      
     } 
     
 	public void startRecording() {
 		if((canRecord)&&(!isRecording)){
         mRecorder = new MediaRecorder();
         mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
         
         Calendar c = Calendar.getInstance();
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
         String currentDateandTime = df.format(c.getTime());
-        lastFile=filePath+"/"+currentDateandTime+".3gp";
+        lastFile=filePath+"/"+currentDateandTime+".mp4";
         
-        tempAudio.add(filePath+"/"+"temp("+recPaused+").3gp");
+        tempAudio.add(filePath+"/"+"temp("+recPaused+").mp4");
         
         mRecorder.setOutputFile(tempAudio.get(0));
-        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
 
         try {
             mRecorder.prepare();
@@ -87,12 +96,12 @@ public class AudioRecorder {
 			if(recPaused==1){
 			mRecorder = new MediaRecorder();
 	        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-	        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+	        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
 	        
-	        tempAudio.add(filePath+"/"+"temp("+recPaused+").3gp");
+	        tempAudio.add(filePath+"/"+"temp("+recPaused+").mp4");
 	        
 	        mRecorder.setOutputFile(tempAudio.get(recPaused));
-	        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+	        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
 
 	        try {
 	            mRecorder.prepare();
@@ -106,24 +115,28 @@ public class AudioRecorder {
 	        isRecPaused = false;
 			}
 			else if(recPaused==2){
+				String tempMerge;
 				try {
-					String tempMerge=mergeAudio(tempAudio.get(0), tempAudio.get(1));
+					tempMerge = mergeAudio(tempAudio.get(0), tempAudio.get(1));
 					recMerged++;
+					deleteFile(tempAudio.get(0));
+					deleteFile(tempAudio.get(1));
 					tempAudio = new ArrayList<String>();
 					tempAudio.add(tempMerge);
 					recPaused--;
-				} catch (IOException e) {
+				} catch (IOException e1) {
 					// TODO Auto-generated catch block
-					e.printStackTrace();
+					e1.printStackTrace();
 				}
+				
 				mRecorder = new MediaRecorder();
 		        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-		        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+		        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
 		        
-		        tempAudio.add(filePath+"/"+"temp("+recPaused+").3gp");
+		        tempAudio.add(filePath+"/"+"temp("+recPaused+").mp4");
 		        
 		        mRecorder.setOutputFile(tempAudio.get(recPaused));
-		        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+		        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
 
 		        try {
 		            mRecorder.prepare();
@@ -143,7 +156,7 @@ public class AudioRecorder {
     }
 	
     public void stopRecording() {
-    	if((isRecording)||(isRecPaused)){
+    	if((isRecording)){
         mRecorder.stop();
         mRecorder.release();
         
@@ -154,8 +167,9 @@ public class AudioRecorder {
         }
         else if(tempAudio.size()==2)
         {        	
-        	try {
-				String tempMerge=mergeAudio(tempAudio.get(0), tempAudio.get(1));
+        	String tempMerge;
+			try {
+				tempMerge = mergeAudio(tempAudio.get(0), tempAudio.get(1));
 				deleteFile(tempAudio.get(0));
 				deleteFile(tempAudio.get(1));
 				recMerged++;
@@ -166,6 +180,7 @@ public class AudioRecorder {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			
         	renameFile(tempAudio.get(0),lastFile);
         	tempAudio=new ArrayList<String>();
         	tempAudio=new ArrayList<String>();
@@ -178,16 +193,65 @@ public class AudioRecorder {
         recPaused=0;
         recMerged=0;
     	}
+    	else if((isRecPaused)){            
+            
+            if(tempAudio.size()==1)
+            {
+            	renameFile(tempAudio.get(0),lastFile);
+            	tempAudio=new ArrayList<String>();
+            }
+            else if(tempAudio.size()==2)
+            {        	
+            	String tempMerge;
+				try {
+					tempMerge = mergeAudio(tempAudio.get(0), tempAudio.get(1));
+					deleteFile(tempAudio.get(0));
+					deleteFile(tempAudio.get(1));
+					recMerged++;
+					tempAudio = new ArrayList<String>();
+					tempAudio.add(tempMerge);
+					recPaused--;
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+            	renameFile(tempAudio.get(0),lastFile);
+            	tempAudio=new ArrayList<String>();
+            	tempAudio=new ArrayList<String>();
+            }         
+            
+            canRecord=true;
+            canPlay=true;
+            isRecording=false;
+            recPaused=0;
+            recMerged=0;
+            isRecPaused=false;
+        	}
     }
     
     public void cancelRecording(){
-    	if((isRecording)||(isRecPaused))
+    	if((isRecording))
     	{
     		mRecorder.stop();
             mRecorder.release();
             mRecorder = null;
             canRecord=true;
             isRecording=false;
+            recPaused=0;
+            recMerged=0;
+    		for(int i=0;i<tempAudio.size();i++){
+    			deleteFile(tempAudio.get(i));
+    		}
+    		tempAudio=new ArrayList<String>();
+    		lastFile=null;
+    		canPlay=false;
+    	}
+    	else if((isRecPaused))
+    	{    		
+            canRecord=true;
+            isRecording=false;
+            isRecPaused=false;
             recPaused=0;
             recMerged=0;
     		for(int i=0;i<tempAudio.size();i++){
@@ -211,28 +275,53 @@ public class AudioRecorder {
 		file.renameTo(file2);
 	}
     
-	private String mergeAudio(String file1,String file2) throws IOException
+	public String mergeAudio(String file1,String file2) throws IOException
 	{
-		FileInputStream fistream1 = new FileInputStream(file1);  // first source file
-        FileInputStream fistream2 = new FileInputStream(file2);//second source file
-        SequenceInputStream sistream = new SequenceInputStream(fistream1, fistream2);
-        FileOutputStream fostream = new FileOutputStream(filePath+"/"+"merge("+recMerged+").3gp");//destinationfile
+		String fileDestination=filePath+"/"+"merge("+recMerged+").mp4";
+		
+		String f1 = file1;
+        String f2 = file2;
+        //String f3 = AppendExample.class.getProtectionDomain().getCodeSource().getLocation().getFile() + "/1365070453555.mp4";
 
-        int temp;
+        Movie[] inMovies = new Movie[]{
+                MovieCreator.build(f1),
+                MovieCreator.build(f2)
+                //MovieCreator.build(f3)
+                };
 
-        while( ( temp = sistream.read() ) != -1)
-        {
-            // System.out.print( (char) temp ); // to print at DOS prompt
-            fostream.write(temp);   // to write to file
+        List<Track> videoTracks = new LinkedList<Track>();
+        List<Track> audioTracks = new LinkedList<Track>();
+
+        for (Movie m : inMovies) {
+            for (Track t : m.getTracks()) {
+                if (t.getHandler().equals("soun")) {
+                    audioTracks.add(t);
+                }
+                if (t.getHandler().equals("vide")) {
+                    videoTracks.add(t);
+                }
+            }
         }
-        fostream.close();
-        sistream.close();
-        fistream1.close();
-        fistream2.close();
-		return filePath+"/"+"merge("+recMerged+").3gp";
+
+        Movie result = new Movie();
+
+        if (audioTracks.size() > 0) {
+            result.addTrack(new AppendTrack(audioTracks.toArray(new Track[audioTracks.size()])));
+        }
+        if (videoTracks.size() > 0) {
+            result.addTrack(new AppendTrack(videoTracks.toArray(new Track[videoTracks.size()])));
+        }
+
+        Container out = new DefaultMp4Builder().build(result);
+
+        randomAccessFile = new RandomAccessFile(String.format(fileDestination), "rw");
+		FileChannel fc = randomAccessFile.getChannel();
+        out.writeContainer(fc);
+        fc.close();
+        
+		return fileDestination;
+
 	}	
-
-
     
     public void startPlaying() {
     	if((canPlay)&&(!isPlaying)){
@@ -277,25 +366,5 @@ public class AudioRecorder {
         isPlayPaused=false;
     	}
     }
-    
-    /*public void pausePlaying(){
-    	if((canPause)&&(isPlaying)){
-    		mPlayer.pause();
-    		playingPausedAt = mPlayer.getCurrentPosition();
-    		canPause=false;
-    		isPlaying=false;
-    		isPlayPaused=true;
-    	}
-    }
-    
-    public void resumePlaying(){
-    	if(isPlayPaused){
-    		mPlayer.seekTo(playingPausedAt);
-    		mPlayer.start();
-    		canPause=true;
-    		isPlayPaused=false;
-    		isPlaying=true;
-    	}
-    }//*/
-    
+
 }
